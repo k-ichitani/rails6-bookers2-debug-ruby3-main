0ad887300151c5fb4a3d0dadf5fe9f137a1417da
k_ichitani <131871278+k-ichitani@users.noreply.github.com>
2023-06-01 09:51:18 +0000

    検索窓start, searches_controllerまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,33 +10,47 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
 
-# 自分がフォローしたりアンフォローしたりするための記述
+# 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-# フォロー一覧を表示するための記述
+# 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
 
-#
+#自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-# フォロワー一覧を表示するための記述
+# 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+
   def follow(user_id)
-    relationships.create(followed_id: user_id)
+    relationships.create(followed_id: user.id)
   end
 
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user.id).destroy
   end
 
   def following?(user)
     followings.include?(user)
   end
 
-
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?", "#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?", "%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?", "%#{word}%")
+    else
+      @user = User.all
+    end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "seaches#seache"
 
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
